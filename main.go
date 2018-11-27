4786dafb5d561bf5367e092efc7f5b28f6d1318e
fangge <18320891182@163.com>
2018-11-27 21:09:41 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/dy-gopkg/kit"
+	"github.com/dy-platform/id-srv-snowflake/logic"
+	"github.com/dy-platform/id-srv-snowflake/idl/platform/id/srv-snowflake"
+)
+
+
+func main(){
+	kit.Init()
+	platform_id_srv_snowflake.RegisterSnowFlakeHandler(kit.DefaultService.Server(), &logic.Handle{})
+	kit.Run()
+}
